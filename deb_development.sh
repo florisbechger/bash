959c75dfceb8c4211d186c25ad6db63b4e3ef63b
florisbechger <69296716+florisbechger@users.noreply.github.com>
2021-01-31 12:26:53 +0100

    Update deb_development.sh

--- deb_development.sh
+++ deb_development.sh
@@ -4,7 +4,7 @@ sudo apt-get install flatpak -y
 sudo apt-get install gnome-software-plugin-flatpak -y
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 sudo flatpak remotes # Flatpak synchronize
-sudo flatpak install filezilla teams zoom -y
+sudo flatpak install filezilla geany libreoffice teams zoom -y
 
 # Install VirtualBox:
 cd ~/Downloads
