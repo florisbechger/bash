cf27715bed507966a3ed143bf362c63505bc6ba6
florisbechger <69296716+florisbechger@users.noreply.github.com>
2021-02-03 15:33:30 +0100

    Update cl_system.sh

--- cl_system.sh
+++ cl_system.sh
@@ -7,6 +7,7 @@ sudo echo "[Time]" >> /etc/systemd/timesyncd.conf
 sudo echo "NTP=nts1.time.nl" >> /etc/systemd/timesyncd.conf
 sudo echo "FallbackNTP=ntp1.time.nl" >> /etc/systemd/timesyncd.conf
 sudo timedatectl set-ntp true
+sudo timedatectl timesync-status
 
 sudo swupd check-update
 sudo swupd update
