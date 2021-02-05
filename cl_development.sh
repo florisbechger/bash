772508995c81dea62133f0fef03d99bdd3773c9c
florisbechger <69296716+florisbechger@users.noreply.github.com>
2021-02-05 15:08:00 +0000

    Update cl_development.sh

--- cl_development.sh
+++ cl_development.sh
@@ -7,9 +7,10 @@ sudo sudo swupd bundle-add kernel-kvm kvm-host # install bundles
 sudo usermod -G kvm -a [username] # add user to group kvm
 sudo systemctl enable libvirtd # enable libvirtd service
 
-sudo -i # change to root
+# Not needed for workstation install:
 
 # create kvm-network:
+sudo -i # change to root
 mkdir /etc/systemd/network/
 # create kvm-network interface:
 echo "[NetDev]" > /etc/systemd/network/br0.netdev
@@ -24,21 +25,17 @@ echo "Address=10.0.0.2/16" >> /etc/systemd/network/br0.network
 echo "Gateway=10.0.0.1" >> /etc/systemd/network/br0.network
 echo "DNS=10.0.0.1" >> /etc/systemd/network/br0.network
 echo "DNS=192.168.0.1" >> /etc/systemd/network/br0.network
-
 # change Name=en* to Name=*0s*:
 sed -i 's/Name=en/Name=*p0s/g' /lib/systemd/network/80-dhcp.network
-
 # configure bridge for ip-tables:
 mkdir /etc/sysctl.d/
 echo "net.bridge.bridge-nf-call-arptables = 0" > /etc/sysctl.d/80-bridge.conf
 echo "net.bridge.bridge-nf-call-ip6tables = 0" >> /etc/sysctl.d/80-bridge.conf
 echo "net.bridge.bridge-nf-call-iptables = 0" >> /etc/sysctl.d/80-bridge.conf
-
 # restart services:
 sudo systemctl restart systemd-networkd
 networkctl
-
 exit # exit root
 
-sudo swupd bundle-add virt-manager-gui
-sudo systemctl status libvirtd
+# sudo swupd bundle-add virt-manager-gui
+# sudo systemctl status libvirtd
