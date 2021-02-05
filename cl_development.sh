8d4ef0bc8a772bc7117d540666449f0a61a56153
florisbechger <69296716+florisbechger@users.noreply.github.com>
2021-02-05 14:20:25 +0000

    Update cl_development.sh

--- cl_development.sh
+++ cl_development.sh
@@ -19,21 +19,22 @@ echo "Kind=bridge" >> /etc/systemd/network/br0.netdev
 echo "[Match]" > /etc/systemd/network/br0.network
 echo "Name=br0" >> /etc/systemd/network/br0.network
 echo "[Network]" >> /etc/systemd/network/br0.network
-echo "DHCP=yes" >> /etc/systemd/network/br0.network
+echo "DHCP=no" >> /etc/systemd/network/br0.network
 echo "Address=10.0.0.2/16" >> /etc/systemd/network/br0.network
 echo "Gateway=10.0.0.1" >> /etc/systemd/network/br0.network
 echo "DNS=10.0.0.1" >> /etc/systemd/network/br0.network
 echo "DNS=192.168.0.1" >> /etc/systemd/network/br0.network
 
-# Change lib/systemd/network/80-dhcp.network 
-# Name=en* to Name=*0s*
+# change Name=en* to Name=*0s*:
+sed -i 's/Name=en/Name=*p0s/g' /lib/systemd/network/80-dhcp.network
 
 # configure bridge for ip-tables:
 mkdir /etc/sysctl.d/
-echo "net.bridge.bridge-nf-call-arptables = 0" > /etc/sysctl.d/90-bridge.conf
-echo "net.bridge.bridge-nf-call-ip6tables = 0" >> /etc/sysctl.d/90-bridge.conf
-echo "net.bridge.bridge-nf-call-iptables = 0" >> /etc/sysctl.d/90-bridge.conf
+echo "net.bridge.bridge-nf-call-arptables = 0" > /etc/sysctl.d/80-bridge.conf
+echo "net.bridge.bridge-nf-call-ip6tables = 0" >> /etc/sysctl.d/80-bridge.conf
+echo "net.bridge.bridge-nf-call-iptables = 0" >> /etc/sysctl.d/80-bridge.conf
 
+# restart services:
 sudo systemctl restart systemd-networkd
 networkctl
 
