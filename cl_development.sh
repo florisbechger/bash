73529417304ba99a3daa7483256a30388d0218f3
florisbechger <69296716+florisbechger@users.noreply.github.com>
2021-02-05 21:21:31 +0000

    Update cl_development.sh

--- cl_development.sh
+++ cl_development.sh
@@ -1,5 +1,5 @@
 
-# Development Packages for vannila ClearLinux
+# Development Packages for vanilla ClearLinux on laptop
 
 # Install KVM for kernel and host:
 sudo swupd update # run update first
@@ -7,7 +7,18 @@ sudo sudo swupd bundle-add kernel-kvm kvm-host # install bundles
 sudo usermod -G kvm -a [username] # add user to group kvm
 sudo systemctl enable libvirtd # enable libvirtd service
 
-# Not needed for workstation install:
+# Basic commands:
+sudo virsh list --all # verify which vm is running
+sudo virsh shutdown [computername] # shutdown vm before cloning
+sudo virt-clone --original [computername] --name [computername][-new] --auto-clone # clone vm
+sudo virsh start [computername][-new] # run clone
+sudo virsh list --all # verify
+
+sudo virsh shutdown [computername] # shutdown vm
+sudo virsh undefine --domain [computername] --storage /var/lib/libvirt/images/[computername].qcow2 --wipe-storage # delete vm & wipe disk
+sudo ls -la /var/lib/libvirt/images/ # verify
+
+# Bare-Server configuration
 
 # create kvm-network:
 sudo -i # change to root
@@ -36,6 +47,3 @@ echo "net.bridge.bridge-nf-call-iptables = 0" >> /etc/sysctl.d/80-bridge.conf
 sudo systemctl restart systemd-networkd
 networkctl
 exit # exit root
-
-# sudo swupd bundle-add virt-manager-gui
-# sudo systemctl status libvirtd
