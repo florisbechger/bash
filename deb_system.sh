df4e20ee1f30266a3d24a3b5108bb77834cb40be
florisbechger <69296716+florisbechger@users.noreply.github.com>
2021-02-07 11:42:08 +0100

    Update deb_system.sh

--- deb_system.sh
+++ deb_system.sh
@@ -26,17 +26,20 @@ sudo pkexec dmesg | grep -i bluetooth
 # Driver firmware:
 sudo apt-get install git -y
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
-cd linux-firmware
+su # change to root
+cd /home/[username]/linux-firmware
 sudo cp iwlwifi-9000-pu-b0-jf-b0-38.ucode /lib/firmware
 sudo mkdir /lib/firmware/intel
 sudo cp intel/ibt-17-16-1.* /lib/firmware/intel
 sudo mkdir /lib/firmware/i915
 sudo cp i915/glk_dmc_ver1_04.bin /lib/firmware/i915
 sudo cp i915/kbl_dmc_ver1_04.bin /lib/firmware/i915
+chmod +x /lib/firmware/i915/*_dmc_ver1_04.bin
 sudo mkdir /lib/firmware/nvidia
 sudo mkdir /lib/firmware/nvidia/gp108
 sudo mkdir /lib/firmware/nvidia/gp108/gr
 sudo cp nvidia/gp108/gr/sw_nonctx.bin /lib/firmware/nvidia/gp108/gr
+chmod +x /lib/firmware/nvidia/gp108/gr/sw_nonctx.bin
 exit
 sudo reboot
 
