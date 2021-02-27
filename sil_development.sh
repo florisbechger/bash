b2e29aa3b2af3d7462a63bb1082c5a32bae6848e
florisbechger <69296716+florisbechger@users.noreply.github.com>
2021-02-27 20:38:47 +0100

    Update sil_development.sh

--- sil_development.sh
+++ sil_development.sh
@@ -1,6 +1,28 @@
 
 # Fedora Silverblue 33 Development
 
+# check toolbox, create and enter your first container:
+toolbox
+toolbox list
+toolbox create -c Test -y # Create Test
+# toolbox enter -c Test
+toolbox rm -f Test -y # Remove Test
+# toolbox rm --force Test
+
+# install flatpak packages:
+sudo flatpak install fedora geany libreoffice -y
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+sudo flatpak remotes
+sudo flatpak install flathub audacious filezilla mediawriter teams zoom -y
+
+# install flatpak updates:
+# sudo flatpak remotes
+# sudo flatpak update
+
+# install cockpit package:
+sudo rpm-ostree install cockpit-ostree cockpit cockpit-machines cockpit-podman cockpit-selinux virt-viewer
+# sudo rpm-ostree install cockpit-storaged PackageKit
+
 # install qemu-kvm:
 sudo rpm-ostree install qemu-kvm libvirt libguestfs-tools virt-install rsync
 sudo systemctl enable libvirtd
