1b76e171dc967e222dbebf380e1963716df9913c
florisbechger <69296716+florisbechger@users.noreply.github.com>
2021-02-27 12:44:50 +0100

    Update sil_system2.sh

--- sil_system2.sh
+++ sil_system2.sh
@@ -9,6 +9,12 @@ sudo rpm-ostree upgrade distro-sync
 # check running version:
 sudo rpm-ostree status
 
+# uninstall Chinese, Japanese and Korean input add-ons (testing):
+sudo rpm-ostree override remove ibus-hangul
+sudo rpm-ostree override remove ibus-libzhuyin
+sudo rpm-ostree override remove ibus-libpinyin
+sudo rpm-ostree override remove ibus-kkc
+
 # enable RPM-Fusion:
 sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
@@ -18,19 +24,13 @@ sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free
 # sudo rpm-ostree rebase fedora:fedora/33/x86_64/silverblue
 
 # install extra packages:
-sudo rpm-ostree install constantine-backgrounds f27-backgrounds-gnome firewall-config gnome-tweak-tool gparted htop NetworkManager-tui neofetch verne-backgrounds-gnome
+sudo rpm-ostree install constantine-backgrounds verne-backgrounds-gnome firewall-config gnome-tweak-tool gparted htop NetworkManager-tui neofetch tlp # thermald allready installed
 
 # create shortcuts:
 # SUPER - B Browser (Firefox)
 # SUPER - F Search
 # SUPER - H Files (Home)
 # SUPER - T Terminal (gnome-terminal)
-
-# uninstall Chinese, Japanese and Korean input add-ons (testing):
-sudo rpm-ostree override remove ibus-hangul
-sudo rpm-ostree override remove ibus-libzhuyin
-sudo rpm-ostree override remove ibus-libpinyin
-sudo rpm-ostree override remove ibus-kkc
 
 # reboot:
 sudo systemctl reboot
