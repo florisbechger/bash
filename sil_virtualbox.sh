0b1b1537f6e6c71f09d5f5f7a319d25640e64c5a
florisbechger <69296716+florisbechger@users.noreply.github.com>
2021-05-07 15:19:34 +0200

    Update sil_virtualbox.sh

--- sil_virtualbox.sh
+++ sil_virtualbox.sh
@@ -1,35 +1,11 @@
 
-# VirtualBox on Fedora Silverblue 33 (NOTE: update needed on VirtualBox!):
+# VirtualBox on Fedora Silverblue 34 (NOTE: no repo needed):
 
-# install virtualbox repo:
+# install virtualBox
 
-cd /etc/yum.repos.d/
-sudo wget https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo
-
-# edit virtualbox repo:
-
-sudo nano /etc/yum.repos.d/virtualbox.repo
-
-# change line and add line:
-
-# baseurl=http://download.virtualbox.org/virtualbox/rpm/fedora/$releasever/$basearch
-baseurl=https://download.virtualbox.org/virtualbox/rpm/fedora/32/$basearch
-
-# remove python 3.9:
-
-sudo rpm-ostree override remove python3.9
-
-# install dependency packages:
-
-sudo rpm-ostree install dkms gcc make python3.8
+sudo wget https://download.virtualbox.org/virtualbox/rpm/fedora/34/x86_64/VirtualBox-6.1-6.1.22_144080_fedora33-1.x86_64.rpm
+sudo rpm-ostree install ./VirtualBox-*.rpm
 
 # reboot:
 
 systemctl reboot
-
-# install virtualbox
-
-sudo rpm-ostree install VirtualBox-6.1
-
-# sudo wget https://download.virtualbox.org/virtualbox/rpm/fedora/32/x86_64/VirtualBox-6.1-6.1.16_140961_fedora32-1.x86_64.rpm
-# sudo rpm-ostree install ./VirtualBox-*.rpm
