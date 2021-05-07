b35e0e3f3304988d3f21ddfa34b15efcac3d2420
florisbechger <69296716+florisbechger@users.noreply.github.com>
2021-05-07 12:43:04 +0200

    Update sil_system2.sh

--- sil_system2.sh
+++ sil_system2.sh
@@ -6,10 +6,11 @@ sudo rpm-ostree override remove ibus-hangul
 sudo rpm-ostree override remove ibus-libzhuyin
 sudo rpm-ostree override remove ibus-libpinyin
 sudo rpm-ostree override remove ibus-kkc
+# sudo rpm-ostree override remove ibus-anthy
 
 # install updates:
 sudo rpm-ostree refresh-md
-sudo rpm-ostree update
+# sudo rpm-ostree update
 sudo rpm-ostree upgrade distro-sync
 
 # check running version:
@@ -17,12 +18,12 @@ sudo rpm-ostree status
 
 # enable RPM-Fusion:
 # sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo rpm-ostree install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-33.noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-33.noarch.rpm
+sudo rpm-ostree install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-34.noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-33.noarch.rpm
 
-# update to version 33:
+# update to version 35:
 # ostree remote list
 # sudo ostree remote gpg-import fedora -k /etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-33-primary
-# sudo rpm-ostree rebase fedora:fedora/33/x86_64/silverblue
+# sudo rpm-ostree rebase fedora:fedora/35/x86_64/silverblue
 
 # install extra packages:
 sudo rpm-ostree install NetworkManager-tui clamav clamtk constantine-backgrounds firewall-config gnome-tweak-tool gparted htop neofetch simple-scan system-config-printer tlp verne-backgrounds-gnome
