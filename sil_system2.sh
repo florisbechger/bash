c4f76cf5df49c32082a1cc357e86846df6a121b2
florisbechger <69296716+florisbechger@users.noreply.github.com>
2021-05-07 14:02:08 +0200

    Update sil_system2.sh

--- sil_system2.sh
+++ sil_system2.sh
@@ -1,5 +1,10 @@
 
-# Fedora Silverblue 33 - Install 2
+# Fedora Silverblue 34 - Install 2
+
+# install updates:
+sudo rpm-ostree refresh-md
+# sudo rpm-ostree update
+sudo rpm-ostree upgrade distro-sync
 
 # uninstall Chinese, Japanese and Korean input add-ons (testing):
 sudo rpm-ostree override remove ibus-hangul
@@ -8,17 +13,12 @@ sudo rpm-ostree override remove ibus-libpinyin
 sudo rpm-ostree override remove ibus-kkc
 # sudo rpm-ostree override remove ibus-anthy
 
-# install updates:
-sudo rpm-ostree refresh-md
-# sudo rpm-ostree update
-sudo rpm-ostree upgrade distro-sync
-
 # check running version:
 sudo rpm-ostree status
 
 # enable RPM-Fusion:
 # sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo rpm-ostree install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-34.noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-33.noarch.rpm
+# sudo rpm-ostree install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-34.noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-33.noarch.rpm
 
 # update to version 35:
 # ostree remote list
