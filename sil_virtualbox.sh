0c20458a37019714b6d12229f8eb407c2568f5a9
florisbechger <69296716+florisbechger@users.noreply.github.com>
2021-05-07 15:29:13 +0200

    Update sil_virtualbox.sh

--- sil_virtualbox.sh
+++ sil_virtualbox.sh
@@ -3,7 +3,8 @@
 
 # install virtualBox
 
-sudo wget https://download.virtualbox.org/virtualbox/rpm/fedora/34/x86_64/VirtualBox-6.1-6.1.22_144080_fedora33-1.x86_64.rpm
+mkdir VirtualBox # This is inside /var/home/[username]!
+wget https://download.virtualbox.org/virtualbox/rpm/fedora/34/x86_64/VirtualBox-6.1-6.1.22_144080_fedora33-1.x86_64.rpm
 sudo rpm-ostree install ./VirtualBox-*.rpm
 
 # reboot:
